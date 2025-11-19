655bff3671b330db1cf133880780775be8b968f5
Kabir <mohammadtansimkabir547@yahoo.com>
2025-11-19 19:55:30 +0000

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,68 @@
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+
+		/*for(int i = 0; i < 100; i++) {
+			System.out.println("I Love You!");			
+		} */
+		/*
+		for(int i = 0; i <= 15; i++) {
+			System.out.println(i);
+		}
+
+		int[] grades = {98, 100, 83, 90, 93};
+
+		for(int i = 0; i < grades.length; i++) {
+			System.out.println(grades[i]);
+		}*/
+
+		/*for(int i = 1; i <= 4; i++) {
+			System.out.println("The Count is: " + i);			
+		}		
+
+		for(int i = 0; i <= 1000; i++) {
+				System.out.println("The Count is: " + i);
+		}*/
+
+		/*String name;
+		name = "Kabir";
+
+		for(int i = 1; i <= 4; i++) {
+			System.out.println(name);
+		}*/
+
+		//for(int i = 20; i >= 1; i--) {
+		//System.out.println("The count is: " + i);
+		//}
+
+		/*int  x  =  0;
+		 for(x=10; x<5; x++)
+		 {
+		 System.out.println("x in loop = "+x);
+		 }*/
+		//System.out.println("x out of loop = "+x);
+
+		//int x = 2;
+
+		//if(x%2 ==0)
+			//System.out.println(x + " is an even number");
+
+		//else 
+			//System.out.println(x + " is an odd number");
+
+		//if (x%2 != 0)
+			//System.out.println(x + " is an odd number");
+
+	/*	for (int x = 0; x<=20; x++)	 
+		{
+		 if (x%2 == 0)
+		 		 
+		 System.out.println(x);
+		}
+		*/ 
+
+
+	}
+
+}
